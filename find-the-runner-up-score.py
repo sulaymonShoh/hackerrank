4ba3a2b94d0842a42b04dbde91e2c3d8039b5358
Sulaymonqori Abdumutallibov <86634428+sulaymonShoh@users.noreply.github.com>
2023-11-22 15:29:01 +0500

    some changes

--- find-the-runner-up-score.py
+++ find-the-runner-up-score.py
@@ -1,4 +1,4 @@
 n = int(input())
 scores = sorted(list(map(int, input().split())))
 
-# print(scores[len(scores)-scores.count(max(scores))-1])
+print(scores[len(scores)-scores.count(max(scores))-1])
